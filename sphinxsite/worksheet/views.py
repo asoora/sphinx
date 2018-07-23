7a08425e6ce2c47761022c58275a064d5ed82d67
Jongseong Woo <22februa@gmail.com>
2018-07-24 00:41:45 +0900

    add new_worksheet.html [issue11]

--- sphinxsite/worksheet/views.py
+++ sphinxsite/worksheet/views.py
@@ -11,3 +11,7 @@ def index(request):
         {'title': '판교고 3학년 2학기 기말고사 대비', 'user': 'sphinx',
             'publisher': '금성출판사(2018)', 'num_questions': 16, 'num_views': 7, 'num_likes': 2, 'num_prints': 1}]
     return render(request, 'worksheet/index.html', {'latest_worksheet_list': latest_worksheet_list})
+
+
+def new_worksheet(request):
+    return render(request, 'worksheet/new_worksheet.html')
